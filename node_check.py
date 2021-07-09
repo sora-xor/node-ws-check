f36ee2349b59d7bc692f6a5d9f6a4b655da3b5ff
pgolovkin <golovkin@soramitsu.co.jp>
2021-07-09 22:46:14 +0300

    Added block check

--- node_check.py
+++ node_check.py
@@ -1,24 +1,38 @@
-import websocket as websocket
+import websocket as ws
 import json
 
-websocket = websocket.create_connection(
-    url='wss://ws.tst.sora2.soramitsu.co.jp', #node endpoint
-    timeout=20, #timeout in seconds
-    max_size=2 ** 32,
-    read_limit=2 ** 32,
-    write_limit=2 ** 32,
-)
+from websocket import WebSocketConnectionClosedException, WebSocketTimeoutException
+
+websocket = None
+try:
+    websocket = ws.create_connection(
+        url='wss://ws.tst.sora2.soramitsu.co.jp', #node endpoint
+        timeout=20, #timeout in seconds
+        max_size=2 ** 32,
+        read_limit=2 ** 32,
+        write_limit=2 ** 32,
+    )
+except (Exception, WebSocketTimeoutException, WebSocketConnectionClosedException):
+    print(408)
+    exit(0)
 
 payload = {
     "jsonrpc": "2.0",
-    "method": "system_chain",
+    "method": "system_syncState",
     "params": [],
     "id": 1
 }
+result = {}
+try:
+    websocket.send(json.dumps(payload))
+    result = json.loads(websocket.recv())
+except WebSocketConnectionClosedException:
+    print(408)
+    exit(0)
 
-websocket.send(json.dumps(payload))
-
-result = json.loads(websocket.recv())
-print(result['result'])
-
+block_diff = result['result']['highestBlock'] - result['result']['currentBlock']
+if block_diff >= 10:
+    print(409)
+else:
+    print(200)
 
